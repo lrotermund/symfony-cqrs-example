6c7ce51864cf4cb6d95f1983a9124965a79c76b5
Lukas Rotermund <lukas.rotermund@live.de>
2022-03-30 16:43:33 +0200

    test red – due to missing CreateUser command

--- tests/Service/UserControllerKernelTest.php
+++ tests/Service/UserControllerKernelTest.php
@@ -3,22 +3,43 @@
 namespace App\Tests\Service;
 
 use App\Controller\UserController;
+use Ecotone\Modelling\CommandBus;
 use Ramsey\Uuid\Uuid;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class UserControllerKernelTest extends KernelTestCase
 {
-    public function testUserCreated()
+    public function testCreateUserReturnsValidUUID()
     {
         self::bootKernel();
 
         $container = static::getContainer();
 
-        $createUserCommand = $container->get(UserController::class);
-        $createUserResponse = $createUserCommand->createUser();
+        $userController = $container->get(UserController::class);
+        $createUserResponse = $userController->createUser();
 
         $userId = $createUserResponse->getContent();
 
         $this->assertTrue(Uuid::isValid($userId));
+    }
+
+    public function testCommandHandlerContainsCreateUserCommand()
+    {
+        self::bootKernel();
+
+        $container = static::getContainer();
+
+        $commandBusMock = $this->getMockBuilder(CommandBus::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $commandBusMock->expects($this->once())
+            ->method('sendWithRouting');
+
+        $container->set(CommandBus::class, $commandBusMock);
+
+        $userController = $container->get(UserController::class);
+
+        $userController->createUser();
     }
 }
